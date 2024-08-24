efa86ccc46f529983327b1ac296c60832fd98505
Pavlos <pavlosvalamontes@gmail.com>
2024-08-24 09:12:20 -0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+let qwerty = document.getElementById("qwerty");
+let phrase = document.getElementById("phrase");
+let buttonReset = document.querySelector("buton");
+
+let missedGuesses = 0;
+
+
+const phrases = [ "Tacos", "Hamburgers", "Chickens", "Hotdogs", "Salmon" ];
+
