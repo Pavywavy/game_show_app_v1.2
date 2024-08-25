4e973114a1e4cf8376fca6c2d83d078965216492
Pavlos <pavlosvalamontes@gmail.com>
2024-08-25 10:40:20 -0400

    Added Arrray and Display functions

--- js/app.js
+++ js/app.js
@@ -1,9 +1,53 @@
 let qwerty = document.getElementById("qwerty");
 let phrase = document.getElementById("phrase");
-let buttonReset = document.querySelector("buton");
+let resetButton = document.querySelector(".btn__reset");
 
-let missedGuesses = 0;
+let missed = 0;
+let match = "null";
+
+//phrases
+
+const phrases = [ 
+    "I love Tacos", 
+    "Hamburgers are great",
+    "Chicken sandwhich is for me",
+    "Hotdogs are interesting",
+    "Salmon is fishy"
+    ];
 
 
-const phrases = [ "Tacos", "Hamburgers", "Chickens", "Hotdogs", "Salmon" ];
+// on click hide start screen overlay
+   resetButton.addEventListener("click", () => {
+        resetButton.style.display = "none";
+   });
 
+   // return a random phrase from an array
+   const getRandomPhraseAsArray = (arr) => {
+        let randomPhrase = Math.floor(Math.random() * arr.length);
+        return arr[randomPhrase].split("");
+   };
+
+   const randomPhrases = getRandomPhraseAsArray(phrases);
+
+   console.log(randomPhrases);
+
+// adds random letter from phrase to display
+   const addPhraseToDisplay = (randomPhrases) => {
+        const list = document.querySelector("ul");
+        for (i = 0; i < randomPhrases.length; i++) {
+            list.insertAdjacentHTML("afterbegin", `<li>${randomPhrases}</li>`);
+            if (randomPhrases.textContent === " ") {
+                document.getElementById("space");
+                randomPhrases.classList.add("space");
+               } else {
+                document.getElementById("letter");
+                randomPhrases.classList.add("letter");
+               }; 
+        };
+   };
+
+  const randomLetters = addPhraseToDisplay(randomPhrases);
+
+function checkLetter(buttonClick) {
+    allTheLetters = randomLetters.getElementById("letter");
+}
